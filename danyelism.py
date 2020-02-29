ca3026b1622243998ce5f48f1eef7526f4b776da
ddvma <60947285+ddvma@users.noreply.github.com>
2020-02-28 22:10:02 -0400

    Add files via upload

---
+++ danyelism.py
@@ -0,0 +1,68 @@
+class Circulo:
+	def registrar_datos(self):
+		self.radio = print("Ingrese el radio del circulo: ")
+
+
+		  def leer_radio(r):
+
+		     if self.radio != 0:
+		     	self.radio = b
+		     elif:
+		     	print("Por favor, ingrese otro valor para la base")
+
+
+	def calcular_area(self):
+		self.area = (self.radio**2*3.14)
+		print("El area del circulo es: ", self.area)
+
+
+class Triangulo:
+	def registrar_datos(self):
+		self.tipo = str(input("Ingrese el tipo de triangulo: "))
+		self.base = print("Ingrese la base del triangulo: ")
+
+		  def leer_base(b):
+
+		  	if self.base != 0:
+		  		self.base = b
+		  	elif:
+		  		print("Por favor, ingrese otro valor para la base")
+
+		self.altura = print("Ingrese la altura del triangulo: ")
+
+		   def leer_altura(h):
+
+		   	if self.altura != 0:
+		   		self.base = h
+		   	elif:
+		   		print("Por favor, ingrese otro valor para la base")
+
+
+	def calcular_area(self):
+		self.area = (self.base*self.altura)/2
+		print("El area del triangulo es: ", self.area)
+
+
+class Rectangulo:
+	def registrar_datos(self):
+		self.base = print("Ingrese la base del rectangulo: ")
+
+		 def leer_base(b):
+		 	if self.base != 0:
+		 		self.base = b
+		 	elif:
+		 		print("Por favor ingrese otro valor para la base")
+
+		self.altura = print("Ingrese la altura del rectangulo: ")
+
+		  def leer_altura(h):
+		  	if h != 0:
+		  		h = self.altura
+		  	elif:
+		  		print("Por favor ingrese otro valor para la altura")
+
+    def calcular_area(self):
+    	self.area = (self.base*self.altura)
+    	print("El area del rectangulo es: ", self.area)
+
+
